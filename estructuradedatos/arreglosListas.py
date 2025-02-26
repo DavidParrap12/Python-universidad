56da18a8f8639198e6686c8794c8e5fcfcf3bf93
David Parra Pardo <98554197+DavidParrap12@users.noreply.github.com>
2025-02-26 16:53:59 -0500

    Add files via upload

---
+++ estructuradedatos/arreglosListas.py
@@ -0,0 +1,14 @@
+arreglo = [
+    ["Cedula", "Nombre", "Apellido", "Edad"],
+    [1, "Juan", "Perez", 25],
+    [2, "Maria", "Gonzalez", 30],
+    [3, "Pedro", "Lopez", 35],
+]
+
+#Para acceder a los elementos
+print(arreglo[1][3])
+#primer corchete fila y segundo columna
+
+arreglo[2] [1] = 80
+print(arreglo)
+
